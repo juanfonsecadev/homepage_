aaa975628cd1e70c2b8640e5cf424463599a000c
Juan Fonseca <juanof2000@gmail.com>
2024-05-10 09:42:50 -0300

    Atualizações no JavaScript para animações do bloco na segunda divisão da landing page e além de atualizações de animação a animação css, correção no estilo de alguns blocos

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
         var animationTopics = document.querySelector('#topics');
-        var posicaoElemento = animationTopics.getBoundingClientRect().top;
+        var posicaoElemento = animationTopics.getBoundingClientRect().bottom;
         var posicaoScroll = window.innerHeight;
 
-        if (posicaoElemento < posicaoScroll) {
-            animationTopics.classList.add('aparecer');
-        } else { 
-            animationTopics.classList.add ('escondido');
+        var posicaoElemento2 = animationTopics.getBoundingClientRect().top; 
+
+        if (posicaoElemento <= posicaoScroll && posicaoElemento2 >= 0) {
+            animationTopics.classList.remove('escondido'); // Remover classe 'escondido' para garantir que o elemento esteja visível
+            animationTopics.classList.add('aparecer'); // Adicionar classe 'aparecer'
+        } else {
+            animationTopics.classList.remove('aparecer'); // Remover classe 'aparecer' para garantir que o elemento não seja visível
+            animationTopics.classList.add('escondido'); // Adicionar classe 'escondido'
         }
     });
 });
